bc369d63ed1c27f56d3e2fb89651bb668e7f11a6
magezee <756591143@qq.com>
2020-05-19 17:27:14 +0800

    实现导航路由

--- react_blog/src/components/LeftPart/index.js
+++ react_blog/src/components/LeftPart/index.js
@@ -1,10 +1,30 @@
+// /src/components/LeftPart/index.js
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
-export default class LeftPart extends Component {
+import {routers}  from '../../routers'
+import './index.less'
+
+const navigation = routers.filter(router => router.isNav === true)      // 过滤掉不是导航用的路由
+
+@withRouter
+class LeftPart extends Component {
+
+    changeRoute = (pathname) =>{
+        this.props.history.push(pathname)
+    }
+
     render() {
         return (
-            <div >
-            这里是左边栏
+            
+            <div className="navigation">
+                {navigation.map(route => {
+                    return <button
+                            key={route.pathname}
+                            onClick={this.changeRoute.bind(this, route.pathname)}    /* 因为要直接往里面传参，因此需要用到bind方法 */
+                        >{route.title}</button>
+                })}              
             </div>
         )}
 }
+export default LeftPart
--- react_blog/src/routers/index.js
+++ react_blog/src/routers/index.js
@@ -1,3 +1,4 @@
+// /src/routers/index.js
 // 统一管理路由并导出
 import {
     Home,
@@ -11,33 +12,46 @@ import {
 
 
 
-export const routers = [
-    {
+export  const routers = [
+    {   
+        title: '首页',
         pathname: '/',
-        viewCompoment:Home,
-        exact:true
+        viewCompoment:Home,     
+        exact:true,
+        isNav:true
     },{
+        title: '404',
         pathname: '/404',
-        viewCompoment: NotFound
+        viewCompoment: NotFound    
     },{
+        title: '关于博客',
         pathname: '/aboutBlog',
-        viewCompoment: AboutBlog,
-        excat:true
+        viewCompoment: AboutBlog,   
+        excat:true,
+        isNav:true
     },{
+        title: '博客文章',
         pathname: '/blogArticle',
-        viewCompoment: BlogArticle,
-        exact:true
+        viewCompoment: BlogArticle,     
+        exact:true,
+        isNav:true
     },{
+        title: '随笔',
         pathname: '/essay',
-        viewCompoment: Essay,
-        exact:true
+        viewCompoment: Essay,       
+        exact:true,
+        isNav:true
     },{
+        title: '留言板',
         pathname: '/messageBoard',
-        viewCompoment: MessageBoard,
-        exact:true
+        viewCompoment: MessageBoard,    
+        exact:true,
+        isNav:true
     },{
+        title: '其他',
         pathname: '/other',
-        viewCompoment: Other,
-        exact:true
+        viewCompoment: Other,       
+        exact:true,
+        isNav:true
     }
 ]
